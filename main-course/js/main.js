ad24a1017f97cf177ba5948cd2a68bba84e7445e
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-01-26 21:45:10 +0100

    Visualise data based on a json

--- main-course/js/main.js
+++ main-course/js/main.js
@@ -4,35 +4,34 @@
 *    2.5 - Activity: Adding SVGs to the screen
 */
 
-var data = [25, 20, 10, 12, 15];
+d3.json("./data/buildings.json").then((data) => {
+    data.forEach(d => d.height = +d.height);
 
-var svg = d3.select("#chart-area").append("svg")
-    .attr("width", 400)
-    .attr("height", 400);
+    var svg = d3.select("#chart-area").append("svg")
+        .attr("width", 400)
+        .attr("height", 400);
 
-var circles = svg.selectAll("circle")
-    .data(data);
+    var circles = svg.selectAll("circle")
+        .data(data);
 
-circles.enter()
-    .append("circle")
-    .attr("cx", (d, idx) => {
-        console.log(`Item ${d} at index ${idx}`);
-        return 25 + idx * 25;
-    })
-    .attr("cy", function (d, idx) {
-        return d + idx * (25 * (Math.random() * 2 + 1));
-    })
-    .attr("r", function (d) {
-        console.log(`Item ${d}`);
-        return d;
-    })
-    .attr("fill", (d) => {
-        var color = [];
+    circles.enter()
+        .append("circle")
+        .attr("cx", (d, idx) => {
+            return 50 + idx * 25;
+        })
+        .attr("cy", function (d, idx) {
+            return 50 + idx * (25 * (Math.random() * 2 + 1));
+        })
+        .attr("r", function (d) {
+            return Math.sqrt(d.height);
+        })
+        .attr("fill", (d) => {
+            if (d.height > 300)
+                return 'rgb(255,0,0)';
+            else if (d.height > 250)
+                return 'rgb(0,255,0)';
+            else
+                return 'rgb(0,0,255';
+        });
+}).catch(error =>{console.log(error)})
 
-        for (let i = 0; i < 3; i++) {
-            color.push(Math.floor(Math.random() * 256));
-        }
-
-        console.log(`Item ${d}`);
-        return 'rgb(' + color.join(',') + ')';
-    });
